fcaf77e33e83086896b7fc8086b97d3aaae10c11
renataapmonteiro <60657822+renataapmonteiro@users.noreply.github.com>
2020-06-16 15:06:47 -0300

    simple component for credit card payment finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
+// import { NgxMaskModule, IConfig } from 'ngx-mask';
+
+// export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
 @NgModule({
   declarations: [
     AppComponent,
     CardFormComponent,
-    InputComponent
+    InputComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // NgxMaskModule.forRoot(options)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DateFormControl } from '../date-form-control'
 
 @Component({
   selector: 'app-card-form',
@@ -19,7 +20,7 @@ export class CardFormComponent implements OnInit {
       Validators.minLength(16),
       Validators.maxLength(16)
     ]),
-    expiration: new FormControl('', [
+    expiration: new DateFormControl('', [
       Validators.required,
       Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
     ]),
@@ -38,6 +39,10 @@ export class CardFormComponent implements OnInit {
 
   onSubmit() {
     console.log('was submitted')
+  }
+
+  onResetClick(){
+    this.cardForm.reset();
   }
 
 }
